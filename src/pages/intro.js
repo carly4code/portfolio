603351b8a16f5d510207ee1c6efcc2c65d752fd4
carly4code <orkwan@hotmail.com.hk>
2023-10-03 20:11:58 -0400

    add coding girl

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -1,6 +1,7 @@
 
-import React  from 'react';
+import React from 'react';
 import TextSlider from '../animations/textSlider';
+import IntoImg from '../image/Coding_Girl.svg';
 // import Sketch from 'react-p5'
 
 const Intro = () => {
@@ -26,23 +27,36 @@ const Intro = () => {
 
   // }
 
-    return (
+  return (
 
-        <section id="home">
-          {/* <Sketch setup={setup} draw={draw} /> */}
-        <div className="pageWrapper homePage1">
-            <div className="intro-text">
+    <section id="home">
+      {/* <Sketch setup={setup} draw={draw} /> */}
+      <div className="pageWrapper homePage1">
+        <div className="intro-text">
           <div className="carly">Carly Or</div>
           <div className="thesth">
             <div className="the">The</div>
-              <div className="role">
-              <TextSlider items={items}/>
-             </div>
+            <div className="role">
+              <TextSlider items={items} />
+            </div>
+
           </div>
+          <div style={{
+            position: 'absolute',
+            right: '12vw',
+            maxWidth: '50%',
+            top: '-164px'
+          }}>
+            <img style={{
+              height: '46vh',
+              objectFit: 'cover'
+            }}
+              src={IntoImg}></img>
           </div>
         </div>
-        </section>
-    );
-  };
+      </div>
+    </section>
+  );
+};
 
-  export default Intro;
+export default Intro;
